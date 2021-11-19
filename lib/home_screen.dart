417dd9d728728bb20a2f943e9745a2405a3c7f94
lamyahamad <439201205@student.ksu.edu.sa>
2021-11-19 20:24:22 +0300

    lamya

---
+++ lib/home_screen.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // main axis alignment : start
+    // cross axis alignment : center
+
+    return Scaffold(
+
+
+    );
+  }
+
+  // when notification icon button clicked
+  void onNotification() {
+    print('notification clicked');
+  }
+}
