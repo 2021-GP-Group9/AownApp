6415de5e952e6ab149086a56500b8dd2f64d2046
LENOVO <439201143@student.ksu.edu.sa>
2021-11-27 21:53:23 +0300

    Back-end code

--- lib/connection/charity_model.dart
+++ lib/connection/charity_model.dart
@@ -12,6 +12,7 @@ class CharityModel {
     required this.description,
     required this.service,
     required this.city,
+    required this.status,
   });
 
   String charityId;
@@ -19,6 +20,7 @@ class CharityModel {
   String description;
   String service;
   String city;
+  String status ;
   String imageString = "";
   late Image image;
 
@@ -30,12 +32,13 @@ class CharityModel {
       imageString = "";
     }
   }
-
+// convert image type from json to map
   factory CharityModel.fromJson(Map<String, dynamic> json) => CharityModel(
     charityId: json["charityId"],
     name: json["name"],
     description: json["descrption"],
     service: json["service"],
     city: json["city"],
+    status: json['status'],
   );
 }
