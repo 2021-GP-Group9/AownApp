d29e91757607b6f30754c8cf1e6c2ca479bc3919
lamyahamad <439201205@student.ksu.edu.sa>
2021-12-04 21:12:32 +0300

    logout and keep logged

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,5 +1,6 @@
 class constant {
   static String url="https://awoon.000webhostapp.com/signup_screen.php";
   static String login_url="https://awoon.000webhostapp.com/login_screen.php";
+ static String profile_url = "https://awoon.000webhostapp.com/fetch_data.php";
       static int responseok=200;
 }
