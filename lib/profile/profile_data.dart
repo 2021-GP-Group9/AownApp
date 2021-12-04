bce7ea07c0556315d614c6536b5e3b75e7c43201
lamyahamad <439201205@student.ksu.edu.sa>
2021-12-05 01:18:00 +0300

    fix same update error

--- lib/profile/profile_data.dart
+++ lib/profile/profile_data.dart
@@ -1,14 +1,14 @@
-// import 'package:flutter/cupertino.dart';
-//
-// class profile_date{
-//   String _name;
-//   int _phone_number;
-//   String _email;
-//   profile_date(this._email,this._name,this._phone_number);
-//
-//   String get email => _email;
-//
-//   String get phone_number => _phone_number;
-//
-//   String get name => _name;
-// }
+import 'package:flutter/cupertino.dart';
+
+class profile_date{
+  String _name;
+  int _phone_number;
+  String _email;
+  profile_date(this._email,this._name,this._phone_number);
+
+  String get email => _email;
+
+  int get phone_number => _phone_number;
+
+  String get name => _name;
+}
