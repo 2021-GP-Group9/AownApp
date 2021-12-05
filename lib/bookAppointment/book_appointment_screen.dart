32952afcc3da8c3d3382b5a9059b684b1ada0681
Aljawharah <439200249@studenr.ksu.edu.sa>
2021-12-05 14:44:44 +0300

    book appointment

--- lib/bookAppointment/book_appointment_screen.dart
+++ lib/bookAppointment/book_appointment_screen.dart
@@ -1,105 +1,268 @@
-
 import 'package:aownapp/home_screen/home_screen.dart';
 import 'package:aownapp/profile/profile_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:intl/date_symbol_data_local.dart';
+import "package:flutter/material.dart";
 import 'package:table_calendar/table_calendar.dart';
+//import 'package:flutter/foundation.dart'
 
 class Book_appointment extends StatelessWidget {
-  const Book_appointment({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "حجز المواعيد ",
+      home: Calendar(),
+    );
+  }
+}
+class Calendar extends StatefulWidget {
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  late Map<DateTime, List<Event>> selectedEvents;
+  CalendarFormat format = CalendarFormat.month;
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
+
+TextEditingController _eventController = TextEditingController();
+
+  @override
+  void initState() {
+    selectedEvents = {};
+    super.initState();
+  }
+
+  List<Event> _getEventsfromDay(DateTime date) {
+    return selectedEvents[date] ?? [];
+  }
+
+  @override
+  void dispose(){
+    _eventController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    //var floatingActionButton;
     return Scaffold(
         appBar: AppBar(
+          title: Text("جدول المواعيد"),
+          centerTitle: true,
           automaticallyImplyLeading: false,
           backgroundColor: const Color(0xffd6daca),
-          actions: [
-
-            Image.asset("assets/finalLogo.jpeg")
-          ],
+          actions: [Image.asset("assets/finalLogo.jpeg")],
         ),
         bottomNavigationBar: Container(
-          color:
-          const Color(0xffD6DACA),
+          color: const Color(0xffD6DACA),
           height: 70,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               GestureDetector(
                   onTap: () {
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Profile()),);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Profile()),
+                    );
                   },
-                  child: Icon(Icons.person,)),
+                  child: Icon(
+                    Icons.person,
+                  )),
               GestureDetector(
                   onTap: () {
-                    Navigator.push(context,
+                    Navigator.push(
+                      context,
                       MaterialPageRoute(
-                          builder: (context) => Book_appointment()),);
+                          builder: (context) => Book_appointment()),
+                    );
                   },
-                  child: Icon(Icons.add_circle, size: 49,)),
+                  child: Icon(
+                    Icons.add_circle,
+                    size: 49,
+                  )),
               GestureDetector(
                   onTap: () {
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => HomeScreen()),);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()),
+                    );
                   },
                   child: Icon(Icons.house))
-
-
             ],
           ),
         ),
         body: Column(
           children: [
             TableCalendar(
+              focusedDay: selectedDay,
               firstDay: DateTime.utc(2021, 10, 16),
               lastDay: DateTime.utc(2030, 3, 14),
-              focusedDay: DateTime.now(),
+              calendarFormat: format,
+              onFormatChanged: (CalendarFormat _format) {
+                setState(() {
+                  format = _format;
+                });
+              },
+              startingDayOfWeek: StartingDayOfWeek.sunday,
+              daysOfWeekVisible: true,
 
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                //changes
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(14.0),
-                    child: Container(
-                      height: 34,
-                      width: 120,
+              //Day changed
+              onDaySelected: (DateTime selectDay, DateTime focusDay) {
+                setState(() {
+                  selectedDay = selectDay;
+                  focusedDay = focusDay;
+                });
+                //test
+                print(focusedDay);
+              },
+              selectedDayPredicate: (DateTime date) {
+                return isSameDay(selectedDay, date);
+              },
+              eventLoader: _getEventsfromDay,
 
-                      decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.all(Radius.circular(6))
-
-                      ),
-                      child: Center(child: Text(
-                        "الغاء", style: TextStyle(color: Colors.white),)),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(14.0),
-                    child: Container(
-                      height: 34,
-                      width: 120,
-
-                      decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.all(Radius.circular(6))
-
-                      ),
-                      child: Center(child: Text(
-                        "حجز موعد", style: TextStyle(color: Colors.white),)),
-                    ),
-                  )
-                ],
+              // To style the Calendar
+              calendarStyle: CalendarStyle(
+                isTodayHighlighted: true,
+                selectedDecoration: BoxDecoration(
+                  color: Colors.green,
+                  //can be change to rectangle as:
+                  // shape: BoxShape.rectangle,
+                  //BorderRadius : BorderRadius.circular(5.0),
+                  shape: BoxShape.circle,
+                ),
+                selectedTextStyle: TextStyle(color: Colors.white),
+                todayDecoration: BoxDecoration(
+                  color: Colors.pink,
+                  //can be change to rectangle as:
+                  // shape: BoxShape.rectangle,
+                  //BorderRadius : BorderRadius.circular(5.0),
+                  shape: BoxShape.circle,
+                ),
+                defaultDecoration: BoxDecoration(
+                  // color: Colors.pink,
+                  //can be change to rectangle as:
+                  // shape: BoxShape.rectangle,
+                  //BorderRadius : BorderRadius.circular(5.0),
+                  shape: BoxShape.circle,
+                ),
+                weekendDecoration: BoxDecoration(
+                  //can be change to rectangle as:
+                  // shape: BoxShape.rectangle,
+                  //BorderRadius : BorderRadius.circular(5.0),
+                  shape: BoxShape.circle,
+                ),
               ),
-            )
-          ],
-        )
-    );
+              headerStyle: HeaderStyle(
+                formatButtonVisible: true,
+                titleCentered: true,
+                formatButtonShowsNext: false,
+                formatButtonDecoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                formatButtonTextStyle: TextStyle(
+                    color: Colors.white,
+                ),
+              ),
+                 ),
+..._getEventsfromDay(selectedDay).map(
+            (Event event) => ListTile(
+                title: Text(
+                    event.title,
+                ),
+            ),
+    ),
+                     Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              //changes
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: Container(
+                    height: 34,
+                    width: 120,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.all(Radius.circular(6))),
+                    child: Center(
+                        child: Text(
+                          "الغاء",
+                          style: TextStyle(color: Colors.white),
+                        )),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: Container(
+                    height: 34,
+                    width: 120,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.all(Radius.circular(6))),
+                    child: Center(
+                        child: Text(
+                          "حجز موعد",
+                          style: TextStyle(color: Colors.white),
+                        )),
+                  ),
+                )
+              ],
+            ),
+          )
+         ],
+        ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text("Add Event"),
+            content: TextFormField(
+              controller: _eventController,
+            ),
+            actions: [
+              TextButton(
+                child: Text("Cancel"),
+                onPressed: () => Navigator.pop(context),
+              ),
+              TextButton(
+                child: Text("Ok"),
+                onPressed: () {
+                  if (_eventController.text.isEmpty) {
+
+                  } else {
+                    if (selectedEvents[selectedDay] != null) {
+                      selectedEvents[selectedDay]!.add(
+                        Event(title: _eventController.text),
+                      );
+                    } else {
+                      selectedEvents[selectedDay] = [
+                        Event(title: _eventController.text)
+                      ];
+                    }
+                  }
+                  Navigator.pop(context);
+                  _eventController.clear();
+                  setState((){});
+                  return;
+                },
+              ),
+            ],
+          ),
+        ),
+        label: Text("Add Event"),
+        icon: Icon(Icons.add),
+      ),
+
+       );
   }
+}
+
+class Event {
+  final String title;
+  Event({required this.title});
+  String toString() => this.title;
 }
